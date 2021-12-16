6995f95b9b98a5d79f8de8a84fee2dbb8f415934
EDEN-NN <walivitorio@gmail.com>
2021-12-16 20:48:15 -0300

    Obtendo um usuário pelo ID

--- src/main/java/com/walison/workshopmongo/services/UserService.java
+++ src/main/java/com/walison/workshopmongo/services/UserService.java
@@ -1,12 +1,14 @@
 package com.walison.workshopmongo.services;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.walison.workshopmongo.domain.User;
 import com.walison.workshopmongo.repository.UserRepository;
+import com.walison.workshopmongo.services.exception.ObjectNotFoundException;
 
 @Service
 public class UserService {
@@ -16,6 +18,11 @@ public class UserService {
 	
 	public List<User> findAll() {
 		return userRepository.findAll();
+	}
+	
+	public User findById(String id) {
+		Optional<User> obj = userRepository.findById(id);
+		return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado!"));
 	}
 	
 }
