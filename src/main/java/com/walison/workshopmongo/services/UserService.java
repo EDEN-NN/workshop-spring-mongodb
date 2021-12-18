016890db12f9d56bab7335dacc970a55aee37812
EDEN-NN <walivitorio@gmail.com>
2021-12-18 14:02:39 -0300

    Inserção de usuário

--- src/main/java/com/walison/workshopmongo/services/UserService.java
+++ src/main/java/com/walison/workshopmongo/services/UserService.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.walison.workshopmongo.domain.User;
+import com.walison.workshopmongo.dto.UserDTO;
 import com.walison.workshopmongo.repository.UserRepository;
 import com.walison.workshopmongo.services.exception.ObjectNotFoundException;
 
@@ -23,6 +24,14 @@ public class UserService {
 	public User findById(String id) {
 		Optional<User> obj = userRepository.findById(id);
 		return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado!"));
+	}
+	
+	public User insert(User obj) {
+		return userRepository.insert(obj);
+	}
+	
+	public User fromDTO(UserDTO objDTO) {
+		return new User(objDTO.getId(), objDTO.getName(), objDTO.getEmail());
 	}
 	
 }
